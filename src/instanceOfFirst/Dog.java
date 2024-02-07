fe947e4f2d054911c00a75700141c435887ff5f4
NasimSursh <suroshzahiri@gmail.com>
2024-02-07 14:26:20 +0100

    instanceof class

---
+++ src/instanceOfFirst/Dog.java
@@ -0,0 +1,16 @@
+package instanceOfFirst;
+
+class Animal{
+	
+}
+
+public class Dog extends Animal{
+
+	public static void main(String[] args) {
+		
+		Dog dog = new Dog();
+		System.out.println(dog instanceof Animal);
+
+	}
+
+}
